d720d0ee44f23cda4bdfc4d5b9b0d7b33dbdb576
marcelo reis <marceloreis_lial@hotmail.com>
2018-07-03 22:25:26 -0300

    adicionado 2 arquivos de exemplo de uso de extrutura condicionais e operadores lógicos

---
+++ tre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear;
+echo "============ TRE ==========";
+echo "";
+echo -n "Digite a sua idade: ";
+read idade;
+### encadeamento if, operadores lógicos e comparadores
+if [ $idade -lt 16 ]; then
+echo "PROIBIDO VOTAR";
+elif [ $idade -ge 18 ] && [ $idade -le 70 ]; then
+echo "OBRIGATORIO VOTAR";
+elif [ $idade -eq 16 ] || [ $idade -eq 17 ] || [ $idade -gt 70 ]; then
+echo "VOTO FACULTATIVO";
+fi
+exit;
